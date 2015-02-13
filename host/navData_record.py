8bf0171ff9348f0558028ccfe30ead2fe88b9433
Adam <amresnick@gmail.com>
2015-02-12 20:58:26 -0800

    Updated to record data over linux serial ports.

--- host/navData_record.py
+++ host/navData_record.py
@@ -2,18 +2,18 @@ import serial
 import time
 
 # set up serial port
-ser = serial.Serial(port=6,baudrate=921600,timeout=0)
+ser = serial.Serial(port='dev/ttyACM0', baudrate=921600, timeout=0)
 
 # create/open an output file
-f = open("navData.txt","wb")
+f = open("imuData.csv", "w")
 
-print "Starting to record."
+print "Starting to record..."
 
-# get data and write into file for 1 second
+# get data and write into file for 10 second
 while time.clock() < 10:
-  data = ser.read()
-  f.write(data)
-  f.flush()
+    data = ser.read()
+    f.write(data)
+    f.flush()
 
 # close file
 f.close()
